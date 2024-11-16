a3ffcbf652f557b2b86bb12a831f58de3ac7c19a
hansel-mikey <98278436+hansel-mikey@users.noreply.github.com>
2024-11-17 01:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,17 +52,29 @@ function switchPlayers() {
   //  so here we have added the color changing property
   player1EL.classList.toggle("player--active");
 }
-
-// this to hide the dice and display it only when the roll the dice is clicked
-diceEl.classList.add("hidden");
 // these are the final scores
-const scores = [0, 0];
+let scores = [];
 // this is to add the current score for the players
 let currentscore = 0;
 // this will hepls to know the active player
 let activePlayer = 0;
 // this i will indicate the game is win or not
-let playing = true;
+let playing;
+
+function init() {
+  // this to hide the dice and display it only when the roll the dice is clicked
+  diceEl.classList.add("hidden");
+  // these are the final scores
+  scores = [0, 0];
+  // this is to add the current score for the players
+  currentscore = 0;
+  // this will hepls to know the active player
+  activePlayer = 0;
+  // this i will indicate the game is win or not
+  playing = true;
+}
+init();
+
 // topic
 // this the btn roll
 // this is for the rooling of the dice
@@ -116,16 +128,17 @@ btnhold.addEventListener("click", function () {
       // this is a fucntion made ti switch the players so when ever the esle block is exucted the the switchPlayers(); functiob us called
       switchPlayers();
     }
-    // now the task is to stop the game when the game is won and the btn should not be working only the new game bt should be working
+    // now the task is to stop the game when the game is won and the btn should not be working only  the new game bt should be working
   }
 });
-
+// topic
 // this is to reset everything
 btnNew.addEventListener("click", function () {
   diceEl.classList.add("hidden");
   socre0();
   currentScore0();
   switchPlayers();
+  init();
   // finally removing the player-winner class
   player0EL.classList.remove("player--winner");
   player1EL.classList.remove("player--winner");
